a706fa5890a611e400281eefea9ad1723cfac12e
mdshayequeahmad <shayeque01@gmail.com>
2022-10-31 23:50:14 +0530

    currying

--- coverage/demo.js
+++ coverage/demo.js
@@ -1,1 +1,21 @@
-console.log('I love Git');
+// let multiply = function(x, y){
+//     console.log(x * y);
+// }
+
+let multiply = function(x){
+    return function (y){
+        console.log(x * y);
+    }
+}
+
+// let multiplyByTwo = multiply.bind(this, 2);
+// multiplyByTwo(3);
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(5);
+
+let multiplyByThree = multiply(3);
+multiplyByThree(10);
